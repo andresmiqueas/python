31b65ea176aadb80502c201f5d91b3d3ab2914ef
andresmiqueas <andresmiqueas27@gmail.com>
2023-01-16 15:59:16 -0300

    tuplas

---
+++ python/05_tuples.py
@@ -0,0 +1,14 @@
+# Tuplas -- conjuunto de valores
+
+
+my_tuple = tuple()
+
+my_other_tuple = ()
+
+my_tuple = (27, 1.79, "Andres", "Gonzalez")
+
+print(my_tuple)
+
+print(type(my_tuple))
+
+print(my_tuple[0])
