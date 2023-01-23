4358c3bcd5635c646e480b04a00be457297c271b
andresmiqueas <andresmiqueas27@gmail.com>
2023-01-23 11:28:22 -0300

    conditionals

---
+++ python/08_conditionals.py
@@ -0,0 +1,11 @@
+# Condicionales
+
+my_condition = 4*3
+
+if my_condition >= 15 or my_condition == 20:
+    print("Se ejecuta el programa")
+elif my_condition == 12: 
+    print("La condicion es iguala 12")
+else:
+    print("No se ejecuta el programa")
+
