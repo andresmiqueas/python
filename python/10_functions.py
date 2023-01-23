1dfbf8d9fbeafd4f06e5413f9efe63966e77ea81
andresmiqueas <andresmiqueas27@gmail.com>
2023-01-23 16:09:49 -0300

    Funciones

---
+++ python/10_functions.py
@@ -0,0 +1,21 @@
+# Funciones 
+
+def my_function():
+    print("Esto es una funcion")
+
+
+my_function()
+
+"""
+def suma(number_one, number_two):
+    print(number_one + number_two)
+
+suma(7,5)
+"""
+
+
+def sum_two_values_with_return(first_value, two_value): # Defino una funcion que recibe dos parametros
+    return first_value + two_value  # Retorna la suma de dos valores
+
+my_resut = sum_two_values_with_return(10, 5) # Defino una variable que contiene el valor de la suma
+print(my_resut) # Printeo el resultado de la suma
