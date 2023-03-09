ccd5cadc5b1519614a612c7ccbb3261a5db59367
andresmiqueas <andresmiqueas27@gmail.com>
2023-03-09 16:05:02 -0300

    listas comprimidas

---
+++ python/Intermedio/01_list_comprehension.py
@@ -0,0 +1,18 @@
+# List Comprehension #
+
+my_original_list = [33, 24, 15, 84, 21, 77, 22]
+
+my_list = [i for i in my_original_list]
+
+print(my_list)
+
+my_range = range(11)
+
+print(list(my_range))
+
+
+my_list_sum = [i + 1 for i in range (11)]
+print(my_list_sum) 
+
+
+
